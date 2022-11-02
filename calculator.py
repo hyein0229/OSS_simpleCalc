e6df092a9bc44b2ec7a76011204f8318049d39e7
hyein0229 <l1253@naver.com>
2022-11-02 17:14:52 +0900

    종료 루프 개선 종료 루프 함수로 분리하여 코드 구조 변경

--- calculator.py
+++ calculator.py
@@ -2,6 +2,27 @@
 
 import calcFunction as cf
 
+def isContinued():
+
+    continued = None  # 진행 여부에 대한 변수
+    while True: 
+        next_calculation = input("Let's do next calculation? (yes/no): ").lower()  # 받은 입력을 소문자로 변경
+        if next_calculation == "yes":  # 계속 진행
+            continued = True
+        elif next_calculation == "no":
+            reply = input("Are you sure? (yes/no)").lower()  # 재확인
+            if reply == "yes":  # 다음 연산 진행 하지 않음
+                continued = False  # 진행하지 않으므로 False 로 변경
+            elif reply == "no":  # 계속 진행
+                continued = True
+
+        if continued is not None:  # 다음 진행 여부가 결정되었다면 루프 탈풀
+            break
+        print("Please answer yes or no.")  # yes/no 이외의 입력이 들어온 모든 경우에 출력됨
+
+    return continued
+
+
 print("Select operation.")
 print("1.Add")
 print("2.Subtract")
@@ -32,11 +53,7 @@ while True:
             else:
                 print(num1, "/", num2, "=", cf.divide(num1, num2))
 
-        # check if user wants another calculation
-        # break the while loop if answer is no
-        next_calculation = input("Let's do next calculation? (yes/no): ")
-        if next_calculation == "no":
+        if not isContinued():  # 종료 여부를 결정할 함수, False 가 리턴되면 계산기 종료 
             break
-
     else:
         print("Invalid Input")
