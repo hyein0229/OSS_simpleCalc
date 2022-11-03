b1a589ed9dc90c87f1f08d83d35a7fb90c986122
hyein0229 <l1253@naver.com>
2022-11-03 17:18:04 +0900

    주석 추가

--- logger.py
+++ logger.py
@@ -4,29 +4,29 @@ class Logger:
 
     def __init__(self, logger_name, file_name, level):
 
-        self.logger = logging.getLogger(logger_name)
-        self.level = level
+        self.logger = logging.getLogger(logger_name)   # 로거 생성
+        self.level = level  # 로그 레벨 설정
 
         if self.level == 1:
             self.logger.setLevel(logging.INFO)
         elif self.level == 2:
             self.logger.setLevel(logging.ERROR)
 
-        log_format = logging.Formatter(fmt = "%(asctime)s - %(levelname)s - %(message)s")
+        log_format = logging.Formatter(fmt = "%(asctime)s - %(levelname)s - %(message)s")  # 출력 형식 설정
 
-        file_handler = logging.FileHandler(filename = file_name)
-        file_handler.setFormatter(log_format)
+        file_handler = logging.FileHandler(filename = file_name)  # 해당 파일로 로그를 출력해줄 파일 핸들러
+        file_handler.setFormatter(log_format)  # 출력 포맷 설정
 
-        self.logger.addHandler(file_handler)
+        self.logger.addHandler(file_handler)  
 
-    def log(self, message):
+    def log(self, message):  # 로그 출력
         if self.level == 1:
             self.logger.info(message)
         elif self.level == 2:
             self.logger.error(message)
 
     def get_stream(self):
-        console = logging.StreamHandler()
+        console = logging.StreamHandler()  # 콘솔에 로그 출력
         self.logger.addHandler(console)
 
 
